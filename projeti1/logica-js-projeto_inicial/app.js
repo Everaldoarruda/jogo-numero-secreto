bd31468c79395f86acffd9c84fa053de9c6414b3
Everaldo arruda <dev.ejmarf@gmail.com>
2025-01-22 19:27:16 -0300

    corrigindo

---
+++ projeti1/logica-js-projeto_inicial/app.js
@@ -0,0 +1,16 @@
+let listaGenerica
+listaGenerica = [];
+console.log(listaGenerica);
+
+let linguagensDeProgramacao;
+linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin','Python'];
+console.log(linguagensDeProgramacao);
+
+linguagensDeProgramacao.push( 'Java', 'Ruby' , 'GoLang');
+
+
+let nomes;
+nomes =['carlos', 'pedro','joao'];
+console.log(nomes[0]);
+console.log(nomes[1]);
+console.log(nomes[2]);
